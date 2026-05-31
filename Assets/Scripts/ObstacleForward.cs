0a2005f57872d1c3ce03bc3484ce274b44326559
paezcarrion21-sudo <paezcarrion21@gmail.com>
2026-05-31 02:45:36 -0500

    cambiso ifnales

---
+++ Assets/Scripts/ObstacleForward.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ObstacleForward : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject obstacleObj;
+    [SerializeField] private Vector3 startPosition;
+    [SerializeField] private Vector3 currTargetPosition;
+    [SerializeField] private float speedRotation;
+
+    private void Start()
+    {
+        startPosition = obstacleObj.transform.position;
+
+    }
+
+    private void Update()
+    {
+        obstacleObj.transform.Rotate(Vector3.forward, speedRotation * Time.deltaTime);
+        if (obstacleObj.transform.position != currTargetPosition)
+        {
+            obstacleObj.transform.position = Vector3.MoveTowards(obstacleObj.transform.position, currTargetPosition, speed * Time.deltaTime);
+        }
+
+
+    }
+}
